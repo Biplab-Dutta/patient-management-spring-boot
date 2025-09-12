68d9d8307dc74e8dd762ab86197757ed4dc74730
Biplab Dutta <biplabdutta860@gmail.com>
2025-09-12 20:12:51 +0545

    add an extension function that converts PatientRequestDTO to Patient entity

--- patient-service/src/main/kotlin/com/zoroxnekko/patientservice/mapper/PatientMapper.kt
+++ patient-service/src/main/kotlin/com/zoroxnekko/patientservice/mapper/PatientMapper.kt
@@ -1,7 +1,9 @@
 package com.zoroxnekko.patientservice.mapper
 
+import com.zoroxnekko.patientservice.dto.PatientRequestDTO
 import com.zoroxnekko.patientservice.dto.PatientResponseDTO
 import com.zoroxnekko.patientservice.model.Patient
+import java.time.LocalDate
 
 fun Patient.toDTO(): PatientResponseDTO {
     return PatientResponseDTO(
@@ -10,5 +12,15 @@ fun Patient.toDTO(): PatientResponseDTO {
         email = email,
         address = address,
         dateOfBirth = dateOfBirth.toString(),
+    )
+}
+
+fun PatientRequestDTO.toEntity(): Patient {
+    return Patient(
+        name = name,
+        email = email,
+        address = address,
+        dateOfBirth = LocalDate.parse(dateOfBirth),
+        registeredDate = LocalDate.parse(registeredDate),
     )
 }
