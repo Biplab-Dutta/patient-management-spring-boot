167cded72fea3ba69aa46575185e76b7e3ddc4f6
Biplab Dutta <biplabdutta860@gmail.com>
2025-09-12 10:59:32 +0545

    chore: initialize patient-service

---
+++ patient-service/src/test/kotlin/com/zoroxnekko/patientservice/PatientServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zoroxnekko.patientservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
