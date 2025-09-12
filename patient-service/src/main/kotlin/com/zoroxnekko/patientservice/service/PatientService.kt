ff9cb4750fa61cc0708645f9fc937166bdac7f66
Biplab Dutta <biplabdutta860@gmail.com>
2025-09-12 20:12:07 +0545

    add a service that creates a patient

--- patient-service/src/main/kotlin/com/zoroxnekko/patientservice/service/PatientService.kt
+++ patient-service/src/main/kotlin/com/zoroxnekko/patientservice/service/PatientService.kt
@@ -1,7 +1,9 @@
 package com.zoroxnekko.patientservice.service
 
+import com.zoroxnekko.patientservice.dto.PatientRequestDTO
 import com.zoroxnekko.patientservice.dto.PatientResponseDTO
 import com.zoroxnekko.patientservice.mapper.toDTO
+import com.zoroxnekko.patientservice.mapper.toEntity
 import com.zoroxnekko.patientservice.repository.PatientRepository
 import org.springframework.stereotype.Service
 
@@ -12,5 +14,10 @@ class PatientService(
     fun getPatients(): List<PatientResponseDTO> {
         val patients = repository.findAll()
         return patients.map { it.toDTO() }
+    }
+
+    fun createPatient(patientRequestDTO: PatientRequestDTO): PatientResponseDTO {
+        val patient = repository.save(patientRequestDTO.toEntity())
+        return patient.toDTO()
     }
 }
