a6e12be9ffddfcbd13a0530513ac436776ce1e0a
Biplab Dutta <biplabdutta860@gmail.com>
2025-09-12 21:12:34 +0545

    make use of Kotlin standard library feature for Map manipulation

--- patient-service/src/main/kotlin/com/zoroxnekko/patientservice/exception/GlobalExceptionHandler.kt
+++ patient-service/src/main/kotlin/com/zoroxnekko/patientservice/exception/GlobalExceptionHandler.kt
@@ -9,12 +9,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException::class)
     fun handleValidationException(ex: MethodArgumentNotValidException): ResponseEntity<Map<String, String>> {
-        val errors = HashMap<String, String>()
-
-        ex.bindingResult.fieldErrors.forEach { error ->
-            errors[error.field] = error.defaultMessage ?: ""
-        }
-
+        val errors = ex.bindingResult.fieldErrors
+            .associate { error -> error.field to (error.defaultMessage ?: "") }
         return ResponseEntity.badRequest().body(errors)
     }
 }
