24fab04b16660e033d1ad2dab025a5d1b93e6b08
Biplab Dutta <biplabdutta860@gmail.com>
2025-09-14 07:54:20 +0545

    add NotBlank validation for String type and NotNull for other in Patient entity

--- patient-service/src/main/kotlin/com/zoroxnekko/patientservice/model/Patient.kt
+++ patient-service/src/main/kotlin/com/zoroxnekko/patientservice/model/Patient.kt
@@ -5,24 +5,25 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
 import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import java.time.LocalDate
-import java.util.UUID
+import java.util.*
 
 @Entity
 class Patient(
     @Id @GeneratedValue
     var id: UUID? = null,
 
-    @field:NotNull
+    @field:NotBlank
     var name: String,
 
-    @field:NotNull
+    @field:NotBlank
     @field:Email
     @Column(unique = true)
     var email: String,
 
-    @field:NotNull
+    @field:NotBlank
     var address: String,
 
     @field:NotNull
