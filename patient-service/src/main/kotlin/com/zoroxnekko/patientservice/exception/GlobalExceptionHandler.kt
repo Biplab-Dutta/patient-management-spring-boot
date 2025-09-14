f93ba61883daaec1ab7128ee5bfe0b24b8d895da
Biplab Dutta <biplabdutta860@gmail.com>
2025-09-14 07:48:50 +0545

    handle exception when patient is not found

--- patient-service/src/main/kotlin/com/zoroxnekko/patientservice/exception/GlobalExceptionHandler.kt
+++ patient-service/src/main/kotlin/com/zoroxnekko/patientservice/exception/GlobalExceptionHandler.kt
@@ -19,10 +19,15 @@ class GlobalExceptionHandler {
 
     @ExceptionHandler(EmailAlreadyExistsException::class)
     fun handleEmailAlreadyExistsException(ex: EmailAlreadyExistsException): ResponseEntity<Map<String, String>> {
-        log.warn("Email address ${ex.message} already exists")
-        val errors = mapOf("message" to "Email address already exists")
-        return ResponseEntity.badRequest().body(errors)
-
+        log.warn(ex.message)
+        val error = mapOf("message" to "Email address already exists")
+        return ResponseEntity.badRequest().body(error)
     }
 
+    @ExceptionHandler(PatientNotFoundException::class)
+    fun handlePatientNotFoundException(ex: PatientNotFoundException): ResponseEntity<Map<String, String>> {
+        log.warn(ex.message)
+        val error = mapOf("message" to "Patient not found")
+        return ResponseEntity.badRequest().body(error)
+    }
 }
