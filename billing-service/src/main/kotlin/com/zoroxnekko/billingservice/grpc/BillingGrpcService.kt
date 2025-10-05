f0ef851be0584a3812b9d5e756867a3ae090f6d9
Biplab Dutta <biplabdutta860@gmail.com>
2025-10-05 12:11:50 +0545

    refactor: update imports in BillingGrpcService.kt

--- billing-service/src/main/kotlin/com/zoroxnekko/billingservice/grpc/BillingGrpcService.kt
+++ billing-service/src/main/kotlin/com/zoroxnekko/billingservice/grpc/BillingGrpcService.kt
@@ -1,5 +1,8 @@
 package com.zoroxnekko.billingservice.grpc
 
+import com.zoroxnekko.billing.grpc.BillingRequest
+import com.zoroxnekko.billing.grpc.BillingResponse
+import com.zoroxnekko.billing.grpc.BillingServiceGrpcKt
 import org.slf4j.LoggerFactory
 import org.springframework.grpc.server.service.GrpcService
 
@@ -8,7 +11,7 @@ class BillingGrpcService : BillingServiceGrpcKt.BillingServiceCoroutineImplBase(
     private val log = LoggerFactory.getLogger(this.javaClass)
 
     override suspend fun createBillingAccount(request: BillingRequest): BillingResponse {
-        log.info("createBillingAccount request received ${request.toString()}")
+        log.info("createBillingAccount request received $request")
 
         return BillingResponse.newBuilder()
             .setAccountId("233")
