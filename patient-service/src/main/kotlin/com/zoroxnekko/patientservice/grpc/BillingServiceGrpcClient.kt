73850a08d49580df2c5445c5e65e60d12fd90ae1
Biplab Dutta <biplabdutta860@gmail.com>
2025-10-05 13:08:00 +0545

    feat: add a billing service gRPC client in patient-service

---
+++ patient-service/src/main/kotlin/com/zoroxnekko/patientservice/grpc/BillingServiceGrpcClient.kt
@@ -0,0 +1,49 @@
+package com.zoroxnekko.patientservice.grpc
+
+import com.zoroxnekko.billing.grpc.BillingRequest
+import com.zoroxnekko.billing.grpc.BillingResponse
+import com.zoroxnekko.billing.grpc.BillingServiceGrpcKt
+import io.grpc.ManagedChannel
+import io.grpc.ManagedChannelBuilder
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import java.io.Closeable
+import java.util.concurrent.TimeUnit
+
+@Service
+class BillingServiceGrpcClient(
+    @Value("\${billing.service.address:localhost}") serverAddress: String,
+    @Value("\${billing.service.grpc.port:9001}") serverPort: Int
+) : Closeable {
+    private val log = LoggerFactory.getLogger(BillingServiceGrpcClient::class.java)
+    private val stub: BillingServiceGrpcKt.BillingServiceCoroutineStub
+    private val channel: ManagedChannel
+
+    init {
+        log.info("Connecting to Billing gRPC service at $serverAddress:$serverPort")
+
+        channel = ManagedChannelBuilder
+            .forAddress(serverAddress, serverPort)
+            .usePlaintext()
+            .build()
+
+        stub = BillingServiceGrpcKt.BillingServiceCoroutineStub(channel)
+    }
+
+    suspend fun createBillingAccount(patientId: String, name: String, email: String): BillingResponse {
+        val request = BillingRequest.newBuilder()
+            .setPatientId(patientId)
+            .setName(name)
+            .setEmail(email)
+            .build()
+
+        val response = stub.createBillingAccount(request)
+        log.info("Received response from billing service via gRPC: $response")
+        return response
+    }
+
+    override fun close() {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS)
+    }
+}
