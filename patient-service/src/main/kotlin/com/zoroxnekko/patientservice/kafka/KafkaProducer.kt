ecd16657c92628a65fb669fd273d584780ac0f05
Biplab Dutta <biplabdutta860@gmail.com>
2025-10-07 08:28:44 +0545

    feat: add a KafkaProducer that dispatches events for patient

---
+++ patient-service/src/main/kotlin/com/zoroxnekko/patientservice/kafka/KafkaProducer.kt
@@ -0,0 +1,29 @@
+package com.zoroxnekko.patientservice.kafka
+
+import com.zoroxnekko.patientevents.kafka.PatientEvent
+import com.zoroxnekko.patientservice.model.Patient
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Service
+
+@Service
+class KafkaProducer(
+    private val kafkaTemplate: KafkaTemplate<String, ByteArray>
+) {
+    private val log = LoggerFactory.getLogger(this.javaClass)
+
+    fun sendEvent(patient: Patient) {
+        val event = PatientEvent.newBuilder()
+            .setPatientId(patient.id.toString())
+            .setName(patient.name)
+            .setEmail(patient.email)
+            .setEventType("PATIENT_CREATED")
+            .build()
+
+        try {
+            kafkaTemplate.send("patient", event.toByteArray())
+        } catch (_: Exception) {
+            log.error("Error sending PatientCreated event: $event")
+        }
+    }
+}
