9163167c276a2a48b557ad0c8d34894da57e2901
Biplab Dutta <biplabdutta860@gmail.com>
2025-10-07 18:05:07 +0545

    wip: trial and error attempts as kafka listener is not working

--- analytics-service/src/main/kotlin/com/zoroxnekko/analyticsservice/AnalyticsServiceApplication.kt
+++ analytics-service/src/main/kotlin/com/zoroxnekko/analyticsservice/AnalyticsServiceApplication.kt
@@ -2,8 +2,10 @@ package com.zoroxnekko.analyticsservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.kafka.annotation.EnableKafka
 
 @SpringBootApplication
+@EnableKafka
 class AnalyticsServiceApplication
 
 fun main(args: Array<String>) {
