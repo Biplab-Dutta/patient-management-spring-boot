2c00c5010a4256a16ac374aced5d54a029f35e01
Biplab Dutta <biplabdutta860@gmail.com>
2025-10-08 13:51:25 +0545

    Revert "wip: trial and error attempts as kafka listener is not working"
    This reverts commit 9163167c276a2a48b557ad0c8d34894da57e2901.

--- analytics-service/src/main/kotlin/com/zoroxnekko/analyticsservice/AnalyticsServiceApplication.kt
+++ analytics-service/src/main/kotlin/com/zoroxnekko/analyticsservice/AnalyticsServiceApplication.kt
@@ -2,10 +2,8 @@ package com.zoroxnekko.analyticsservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.kafka.annotation.EnableKafka
 
 @SpringBootApplication
-@EnableKafka
 class AnalyticsServiceApplication
 
 fun main(args: Array<String>) {
